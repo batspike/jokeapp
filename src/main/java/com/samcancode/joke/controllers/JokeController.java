959ca28f38d69831b8f92a9c793c7bb08e247b76
Sam Lau <samlau.sam@gmail.com>
2020-07-27 22:50:03 +0800

    added JokeController.

---
+++ src/main/java/com/samcancode/joke/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package com.samcancode.joke.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.samcancode.joke.services.JokeService;
+
+@Controller
+public class JokeController {
+	private JokeService jokeService;
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/",""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "chucknorris";
+	}
+}
