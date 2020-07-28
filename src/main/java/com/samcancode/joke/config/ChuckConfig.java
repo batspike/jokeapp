af14e81d6ca9b8f63cab9aafa79fc214e9cc7458
Sam Lau <samlau.sam@gmail.com>
2020-07-28 16:24:23 +0800

    added java config and refactor service to use bean.

---
+++ src/main/java/com/samcancode/joke/config/ChuckConfig.java
@@ -0,0 +1,14 @@
+package com.samcancode.joke.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfig {
+	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
+}
